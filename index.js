77137192913a722aa22e08f72a25f95fe3e670f7
yun5581 <lonnie513@ewhain.net>
2022-05-20 15:17:02 +0900

    html이랑 css 틀 짰음... 자바스크립트가 문제로다

--- index.js
+++ index.js
@@ -2,62 +2,94 @@ const form = document.querySelector(".todo-form");
 
 const init = () => {
   form.addEventListener("submit", addTodoItem);
+  // form.addEventListener("submit", addTodoContent);
 };
 
-// 입력된 타이틀을 todo list에 추가
-const addTodoTitle= () => {
+const button = document.querySelector(".todo-input-button");
+
+// 입력된 title 을 todo title list에 추가
+const addTodoItem = () => {
   event.preventDefault();
   const todoTitle = document.querySelector(".todo-input-title").value;
-  if (todoTitle) printTodoItem(todoTitle);
+  const todoContent = document.querySelector(".todo-input-content").value;
+  if (todoTitle) printTodoItem(todoTitle,todoContent);
 };
 
-// 입력된 내용을 todo list에 추가
-const addTodoContent= () => {
-  event.preventDefault();
-  const todoContent = document.querySelector(".todo-input-content").value;
-  if (todoContent) printTodoItem(todoContent);
-};
+// // 입력된 content 을 content list에 추가
+// const addTodoContent = () => {
+//   event.preventDefault();
+//   const todoContent = document.querySelector(".todo-input-content").value;
+//   if (todoContent) printTodoItem(todoContent);
+// };
 
 // 화면에 todo item을 추가
-const printTodoItem = text => {
-    const newTodo = document.createElement("li");
-    const newTodoTitle = document.createElement("span");
-    const newTodoContent = document.createElement("span");
-    const todoDel = document.createElement("button");
+const printTodoItem = (title,content) => {
+  const newTodo = document.createElement("div");
+  const newTodoTitle = document.createElement("span");
+  const newTodoContent = document.createElement("span");
+  // const todoDel = document.createElement("button");
 
-  // todo item 텍스트 추가
-  newTodoTitle.innerText = text;
+  // todo title 텍스트 추가
+  newTodoTitle.innerText = title;
   newTodoTitle.className = "todo-item-title";
-  newTodoTitle.addEventListener("click", toggleTodoTitleToDone);
+  // newTodoTitle.addEventListener("click", toggleTodoToDone);
 
-  newTodoContent.innerText = text;
+  // todo content 텍스트 추가
+  newTodoContent.innerText = content;
   newTodoContent.className = "todo-item-content";
-  newTodoContent.addEventListener("click", toggleToContentToDone);
+  // newTodoContent.addEventListener("click", toggleTodoToDone);
 
-  // todo item 삭제 버튼 추가
-  todoDel.innerText = "-"; 
-  todoDel.addEventListener("click", deleteTodoItem);
+
+  // // todo item 삭제 버튼 추가
+  // todoDel.innerText = "삭제";
+  // todoDel.className = "todo-delete-button";
+  // todoDel.addEventListener("click", deleteTodoItem);
 
   // li에 item 추가
   newTodo.className = "todo-list-item";
   newTodo.appendChild(newTodoTitle);
   newTodo.appendChild(newTodoContent);
-  newTodo.appendChild(todoDel);
+  // newTodo.appendChild(todoDel);
 
-  document.querySelector(".todo-list").appendChild(newTodo);
-  document.querySelector(".todo-input-title").value = "";
-  document.querySelector(".todo-input-content").value = "";
+  
 };
 
-const deleteTodoItem = e => {
-  const target = e.target.parentNode;
-  document.querySelector(".todo-list").removeChild(target);
-};
+// const deleteTodoItem = e => {
+//     const target = e.target.parentNode;
+//     document.querySelector(".todo-list").removeChild(target);
+// };
 
-const toggleDoneToTodo = e => {
-  deleteDoneItem(e);
-  printTodoItem(e.target.innerText);
-};
+// const toggleTodoToDone = e => {
+//     deleteTodoItem(e);
+//     printDoneItem(e.target.innerText);
+// }
+
+// const printDoneItem = text => {
+//     const newTodoDone = document.createElement("li");
+//     const newTodoText = document.createElement("span");
+
+//     newTodoText.innerText = text;
+//     newTodoText.className = "done-item-text";
+//     newTodoText.addEventListener("click", toggleDoneToTodo);
+
+//     newTodoDone.className ="done-list-item";
+//     newTodoDone.appendChild(newTodoText);
+
+//     document.querySelector(".done-list").appendChild(newTodoDone);
+// };
+
+// const deleteDoneItem = e => {
+//     const target = e.target.parentNode;
+//     document.querySelector(".done-list").removeChild(target);
+// };
+
+// const toggleDoneToTodo = e => {
+//     deleteDoneItem(e);
+//     printTodoItem(e.target.innerText);
+// };
 
 
 init();
+document.querySelector(".todo-list").appendChild(newTodo);
+document.querySelector(".todo-input-title").value = "";
+document.querySelector(".todo-input-content").value = "";
