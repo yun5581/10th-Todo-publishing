18990063586ead868bc390e70e359c902d0446d5
yun5581 <lonnie513@ewhain.net>
2022-05-24 17:17:37 +0900

    자바함수 구현 중

--- new.js
+++ new.js
@@ -5,15 +5,17 @@ const init = () => {
 };
 
 const addTodoItem = () => {
+    event.preventDefault();
     const todoTitle = document.querySelector(".todo-input-title").value;
-    const todoContent = document.querySelector(".todo-input-content").values;
-    if (todoTitle) printTodoItem(todoContent,todoTitle);
+    const todoContent = document.querySelector(".todo-input-content").value;
+    if (todoTitle) printTodoItem(todoTitle, todoContent);
 };
 
-const printTodoItem = text => {
-    const newTodo = document.createElement("a");
+const printTodoItem = (title, content) => {
+    
+    const newTodo = document.createElement("li");
     const newTodoText = document.createElement('div');
-    const newToddTitle = document.createElement("span");
+    const newTodoTitle = document.createElement("span");
     const newTodoContent = document.createElement("span");
     // const todoDel = document.createElement("button");
 
@@ -26,35 +28,46 @@ const printTodoItem = text => {
     newTodoContent.innerText = content;
     newTodoContent.className = 'todo-content';
 
-    // todo item checkbox 버튼 추가
-    todoCheck.type = 'checkbox';
-    todoCheck.className = 'todoTodone-button';
-    todoCheck.addEventListener('click', function () {
-    const is_checked = todoCheck.checked;
-    if (is_checked) {
-        printDoneItem(title, content);
-    } else {
-        newTodoTitle.style.cssText = 'font-size:30px;font-weight: bold;';
-        newTodoContent.style.cssText =
-        'color: #8b8b8b;font-weight: Regular;font-size: 20px;';
-    }
-    });
+    // // todo item checkbox 버튼 추가
+    // todoCheck.type = 'checkbox';
+    // todoCheck.className = 'todoTodone-button';
+    // todoCheck.addEventListener('click', function () {
+    // const is_checked = todoCheck.checked;
+    // if (is_checked) {
+    //     printDoneItem(title, content);
+    // } else {
+    //     newTodoTitle.style.cssText = 'font-size:30px;font-weight: bold;';
+    //     newTodoContent.style.cssText =
+    //     'color: #8b8b8b;font-weight: Regular;font-size: 20px;';
+    // }
+    // });
 
     newTodoText.appendChild(newTodoTitle);
-    newTodoText.appendChild(br);
+    // newTodoText.appendChild(br);
     newTodoText.appendChild(newTodoContent);
 
-    newTodoBtn.appendChild(todoCheck);
+    // newTodoBtn.appendChild(todoCheck);
     // newTodoBtn.appendChild(todoDel);
 
-    newTodo.className = 'todo-list';
-    newTodo.appendChild(newTodoTitle);
-    newTodo.appendChild(newTodoContent);
+    newTodo.className = 'todo-list-item';
+    newTodo.appendChild(newTodoText);
 
-    document.querySelector('.todo-list').appendChild(newTodo);
-    document.querySelector('.todo-input').value = '';
-    document.querySelector('.todo-title').value = '';
-    document.querySelector('.todo-content').value = '';
-}
+    // document.getElementById(".tb1-title").appendChild(newTodoTitle);
+    document.querySelector('.todobox-title-1').appendChild(newTodoTitle);
+    document.querySelector('.todobox-content-1').appendChild(newTodoContent);
+    // document.querySelector('.todo-list').appendChild(newTodo);
+    document.querySelector('.todo-input-title').value = '';
+    document.querySelector('.todo-input-content').value = '';
+};
+// 박스 나타내기
+// $(function(){
+// 	if("조건1"){
+// 		document.getElementById("").style.display = "none";
+// 		document.getElementById("b_div").style.display = "";
+// 	}else{//조건 1이 아닐떄
+// 		document.getElementById("a_div").style.display = "";
+//         document.getElementById("b_div").style.display = "none";
+// 	}
+// })
 
 init();
